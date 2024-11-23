b968e78bdf9995fb453ac474cb3ca9f29db0c489
Juan Sebastian Lopez <juans.lopez2004@gmail.com>
2024-11-22 20:51:03 -0500

    Initial commit

---
+++ src/main/java/com/sebaslv/SebaslvApplication.java
@@ -0,0 +1,13 @@
+package com.sebaslv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebaslvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebaslvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebaslv/SebaslvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebaslv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebaslvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
